3ae01d3a492ee76ae941178f067a15b3fa2964cf
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-05-23 10:33:23 +0200

    add Bot with some changes to the code that was provided to us.

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,20 +1,61 @@
 package org.example;
 
+import org.example.chat_gpt_datastructures.chat_gpt_datastructures.ChatGPTService;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.List;
+
 
 public class Bot extends TelegramLongPollingBot {
+
     @Override
     public void onUpdateReceived(Update update) {
+        long chatId = update.getMessage().getChatId();
+        String messageReceived = update.getMessage().getText();
+        System.out.println(messageReceived);
 
+        // start to evaluate the messages you received
+        // 1. Main menu
+        if (messageReceived.toLowerCase().startsWith("hello")) {
+            sendResponse(chatId, "Welcome to the ChatGPT telegramm integration");
+            sendResponse(chatId, "Feel free to ask ChatGPT anything");
+            sendResponse(chatId, """
+                    1. Begin the question/message with 'chatgpt' to let it get answered by ChatGPT
+                    For Example: chatgpt How do I write a Hello World program in Java?
+                    Alternatively end the message with a question mark '?'
+                   """ );
+        }
+
+        if(messageReceived.toLowerCase().startsWith("chatgpt") || messageReceived.endsWith("?")){
+            ChatGPTService service = new ChatGPTService();
+            sendResponse(chatId,service.postPrompt(messageReceived));
+
+
+        }
+        else
+
+        // 4. analyze the text
+        {
+            sendResponse(chatId,"");
+        }
     }
 
-    @Override
-    public void onUpdatesReceived(List<Update> updates) {
-        super.onUpdatesReceived(updates);
+
+
+    private void sendResponse(long chatId, String s) {
+        SendMessage msg = new SendMessage();
+        msg.setChatId(chatId);
+        msg.setText(s);
+
+        try {
+            execute(msg);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
+
     @Override
     public String getBotToken(){
         return System.getenv("BOT_TOKEN");
@@ -24,8 +65,4 @@ public class Bot extends TelegramLongPollingBot {
         return System.getenv("BOT_NAME");
     }
 
-    @Override
-    public void onRegister() {
-        super.onRegister();
-    }
 }
