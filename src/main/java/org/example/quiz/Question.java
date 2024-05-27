09a62407772102919a148653e750942ae12782fc
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-05-27 18:51:10 +0200

    change instance-variable title to category, since the api doesn't provide titles for their question. Todo: change the toString() method for a more fitting question message.

--- src/main/java/org/example/quiz/Question.java
+++ src/main/java/org/example/quiz/Question.java
@@ -4,13 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Question {
-    private String title;
+    private String category;
     private String text;
     private List<String> options;
     private String solution;
 
-    public Question(String title, String text, List<String> options, String solution) {
-        this.title = title;
+    public Question(String category, String text, List<String> options, String solution) {
+        this.category = category;
         this.text = text;
         this.options = options;
         this.solution = solution;
@@ -18,9 +18,13 @@ public class Question {
 
     public String toString() {
         String opt = this.options.stream().collect(Collectors.joining("\n"));
-        return this.title + "\n" + this.text + "\n" + opt;
+        return this.category + "\n" + this.text + "\n" + opt;
     }
     public String getSolution(){
         return this.solution;
+    }
+
+    public List<String> getOptions(){
+        return options;
     }
 }
