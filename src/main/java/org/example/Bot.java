1bcc05600afe627b416927b5bd6da059e6b0ae0a
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-05-27 13:59:51 +0200

    add Main menu after writing hello or /start

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -20,7 +20,7 @@ public class Bot extends TelegramLongPollingBot {
     private State state = State.DEFAULT;
     private Quiz quiz = new Quiz();
     private Question question = quiz.getRandomQuestion();
-    private InlineKeyboardButton button = new InlineKeyboardButton("button");
+
 
 
 
@@ -28,21 +28,42 @@ public class Bot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
 
         long chatId = 0;
+
         String answerToQuestion = "";
+
         String messageReceived = "";
 
-        if(update.hasCallbackQuery()){
+        String startUpMessage = "";
+
+        boolean hasChosenMenuOptions = update.hasCallbackQuery() && (update.getCallbackQuery().getData().equals("chat-gpt")
+                                                                 || update.getCallbackQuery().getData().equals("quiz"));
+        boolean isTextMessage = update.hasMessage() && update.getMessage().hasText();
+
+        boolean isAnswerToQuiz = update.hasCallbackQuery() && state.equals(State.QUIZ);
+        
+        if(hasChosenMenuOptions){
+
+            chatId = update.getCallbackQuery().getMessage().getChatId();
+            startUpMessage = update.getCallbackQuery().getData();
+        }
+        if(isAnswerToQuiz){
 
             chatId = update.getCallbackQuery().getMessage().getChatId();
             answerToQuestion = update.getCallbackQuery().getData();
         }
 
-        if(update.hasMessage() && update.getMessage().hasText()) {
+        if(isTextMessage) {
 
             chatId = update.getMessage().getChatId();
             messageReceived = update.getMessage().getText();
         }
 
+        boolean isCallForMainMenu =  (messageReceived.toLowerCase().startsWith("hello")
+                || messageReceived.toLowerCase().startsWith("/start"));
+
+        boolean isCallForQuiz = messageReceived.toLowerCase().startsWith("quiz") || startUpMessage.equals("quiz");
+
+        boolean isCallForChatGpt = messageReceived.toLowerCase().startsWith("chatgpt") || startUpMessage.equals("chat-gpt");
 
         if(state.equals(State.QUIZ)){
 
@@ -51,6 +72,7 @@ public class Bot extends TelegramLongPollingBot {
             if(hasAnsweredCorrect(answerToQuestion)) {
 
                 question = quiz.getRandomQuestion();
+
                 sendResponse(chatId, "Correct!");
                 sendResponse(chatId,question.toString());
                 sendInlineQuizMenu(chatId);
@@ -72,27 +94,26 @@ public class Bot extends TelegramLongPollingBot {
         // /start to evaluate the messages you received
         // 1. Welcoming text that explains the features in a chat message
         // after a text that starts with hello got send
-        if (state.equals(State.DEFAULT) && (messageReceived.toLowerCase().startsWith("hello")
-                                        || messageReceived.toLowerCase().startsWith("/start"))) {
+        if (isCallForMainMenu) {
 
             helloResponse(chatId);
-            sendResponse(chatId,button.getText());
+
             return;
         }
 
-        if(state.equals(State.DEFAULT) && messageReceived.toLowerCase().startsWith("quiz")){
+        if(isCallForQuiz){
 
             quizStarter(chatId);
             return;
         }
 
-        if(messageReceived.toLowerCase().startsWith("chatgpt")){
+        if(isCallForChatGpt){
 
             state = State.CHAT_GPT;
             sendResponse(chatId,"You are now in chatgpt mode");
         } else //If nothing of the above is done, this will get send.
         {
-            sendResponse(chatId,"");
+            sendResponse(chatId,"Write 'hello' or '/start' to open the main-menu");
         }
     }
 
@@ -109,44 +130,6 @@ public class Bot extends TelegramLongPollingBot {
         sendResponse(chatId, question.toString());
         sendInlineQuizMenu(chatId);
     }
-
-    void helloResponse(long chatId){
-        sendResponse(chatId, """
-                    0. You are currently in default mode, apart from messages that start with hello,
-                        only the mentioned keyword in the other bullet-points will have an effect on the bot.
-                    1. Type a message starting with 'chatgpt' to get into the chatgpt mode,
-                        every following inputs will be send to chatgpt.
-                    2. If you send a message starting with 'quiz' you will get into the quiz-mode and
-                        a question will appear directly after you send the message. If you answer it successfully
-                        you will get another question until you leave the mode.
-                        To leave any mode just type a message starting with 'quit'
-                   """ );
-    }
-
-    void chatGPTResponse(long chatId, String messageReceived){
-        if(messageReceived == null) return;
-        quit(messageReceived);
-        ChatGPTService service = new ChatGPTService();
-        sendResponse(chatId,service.postPrompt(messageReceived));
-    }
-    void quit(String messageReceived){
-        if(messageReceived == null) return;
-        if(!messageReceived.toLowerCase().startsWith("quit")) return;
-        this.state = State.DEFAULT;
-    }
-
-    private void sendResponse(long chatId, String s) {
-        SendMessage msg = new SendMessage();
-        msg.setChatId(chatId);
-        msg.setText(s);
-
-        try {
-            execute(msg);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     private void sendInlineQuizMenu(long chatId){
         SendMessage message = new SendMessage();
@@ -189,6 +172,78 @@ public class Bot extends TelegramLongPollingBot {
             e.printStackTrace();
         }
     }
+
+    void helloResponse(long chatId){
+        sendResponse(chatId, """
+                    0. You are currently in default mode, apart from messages that start with hello,
+                        only the mentioned keyword in the other bullet-points will have an effect on the bot.
+                    1. Type a message starting with 'chatgpt' to get into the chatgpt mode,
+                        every following inputs will be send to chatgpt.
+                    2. If you send a message starting with 'quiz' you will get into the quiz-mode and
+                        a question will appear directly after you send the message. If you answer it successfully
+                        you will get another question until you leave the mode.
+                        To leave any mode just type a message starting with 'quit'
+                   """ );
+        sendStartMenu(chatId);
+    }
+
+    void chatGPTResponse(long chatId, String messageReceived){
+        if(messageReceived == null) return;
+        quit(messageReceived);
+        ChatGPTService service = new ChatGPTService();
+        sendResponse(chatId,service.postPrompt(messageReceived));
+    }
+    void quit(String messageReceived){
+        if(messageReceived == null) return;
+        if(!messageReceived.toLowerCase().startsWith("quit")) return;
+        this.state = State.DEFAULT;
+    }
+
+    private void sendResponse(long chatId, String s) {
+        SendMessage msg = new SendMessage();
+        msg.setChatId(chatId);
+        msg.setText(s);
+
+        try {
+            execute(msg);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendStartMenu(long chatId){
+
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText("Choose an option: ");
+
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        List<InlineKeyboardButton> rowInline = new ArrayList<>();
+        List<InlineKeyboardButton> rowInline2 = new ArrayList<>();
+
+        InlineKeyboardButton btn1 = new InlineKeyboardButton();
+        btn1.setText("Quiz");
+        btn1.setCallbackData("quiz");
+        rowInline.add(btn1);
+
+        InlineKeyboardButton btn2 = new InlineKeyboardButton();
+        btn2.setText("Chat-Gpt");
+        btn2.setCallbackData("chat-gpt");
+        rowInline2.add(btn2);
+
+        rowsInline.add(rowInline);
+        rowsInline.add(rowInline2);
+        markupInline.setKeyboard(rowsInline);
+        message.setReplyMarkup(markupInline);
+
+        try{
+            execute(message);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+
 
 
     //Methods mainly for test-purposes, that's why the visibility is default.
